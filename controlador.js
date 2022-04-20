a5059d0b609008eab2597ab0aee741a085fb80dc
Julian Villada <a@gmail.com>
2022-04-20 16:41:39 -0500

    se aprende sobre arreglos de variables

---
+++ controlador.js
@@ -0,0 +1,12 @@
+let numeros=Array(1,2,3,4,5,6,7,8,9,10)
+console.log(numeros)
+console.log(numeros[7])
+let nombres=Array("Amparo","Pabon","Andres","Juan","Julian","Tomas","Sebastian","Esteban","Barbara","Miguel")
+console.log(nombres)
+console.log(nombres[2])
+let edad=Array(1,2,3,4,5,66,22,33,12)
+console.log(edad)
+console.log(edad[3])
+for(let i=0 ; i<nombres.length ; i++){
+    console.log(nombres[i], edad[3])
+}
